1241bd3a585f10dc4da2aec9b859acdbdc8df4b9
pitch <95540893+pitch2@users.noreply.github.com>
2023-07-20 10:33:53 +0200

    Update ip.py

--- ip.py
+++ ip.py
@@ -2,8 +2,13 @@ import subprocess
 import concurrent.futures
 import socket
 
+'''
 ip_dep = input("Début de la plage à analyser : ")
 ip_fin = input("Fin de la plage à analyser : ")
+'''
+
+ip_dep = "192.168.1.1"
+ip_fin = "192.168.1.25"
 
 
 def get_hostname(ip_address):
@@ -34,20 +39,27 @@ def multi_requete(liste):
 result_ip_dep = requete(ip_dep)
 print(result_ip_dep)
 
+result_ip_dep = requete(ip_dep)
+print(result_ip_dep)
+
 ip = ip_dep
+ip_fin_parts = list(map(int, ip_fin.split('.')))  # Convertir l'adresse IP de fin en une liste d'entiers
+
 while ip != ip_fin:
     listeip = []
-    for _ in range(255):
-        ip_parts = list(map(int, ip.split('.')))
-        if ip_parts[3] < 255 and ip_fin != f"{ip_parts[0]}.{ip_parts[1]}.{ip_parts[2]}.{ip_parts[3]+1}":
-            ip_parts[3] += 1
-        else:
-            ip_parts[3] = 0
-            ip_parts[2] += 1
+    ip_parts = list(map(int, ip.split('.')))  # Convertir l'adresse IP en une liste d'entiers
 
-        ip = ".".join(map(str, ip_parts))
+    while ip_parts[2] <= ip_fin_parts[2] and ip != ip_fin:  # Vérifier si le 3e octet est inférieur ou égal à celui de l'adresse IP de fin
+        for _ in range(255):
+            if ip_parts[3] >= ip_fin_parts[3] or ip == ip_fin:
+                break
+            elif ip_parts[3] < 255 and ip != f"{ip_parts[0]}.{ip_parts[1]}.{ip_parts[2]}.{ip_parts[3]+1}":
+                ip_parts[3] += 1
+            else:
+                ip_parts[3] = 0
+                ip_parts[2] += 1
 
-        listeip.append(ip)
-    
+            ip = ".".join(map(str, ip_parts))
+            listeip.append(ip)
 
     multi_requete(listeip)
