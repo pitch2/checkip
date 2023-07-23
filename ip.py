6816327c833192c7b94eb4c350640a34cb7f40ea
pitch <95540893+pitch2@users.noreply.github.com>
2023-07-23 21:38:55 +0200

    Update ip.py

--- ip.py
+++ ip.py
@@ -2,21 +2,18 @@ import subprocess
 import concurrent.futures
 import socket
 
-f = open("scanip.txt","w" )
-f.close
-
+f = open("scanip.txt", "w")
+f.close()
 
 ip_dep = input("Début de la plage à analyser : ")
 ip_fin = input("Fin de la plage à analyser : ")
 
-'''
-ip_dep = "192.168.1.1"
-ip_fin = "192.168.1.25"
-'''
-
 def get_hostname(ip_address):
-    hostname = socket.gethostbyaddr(ip_address)
-    return hostname[0]
+    try:
+        hostname = socket.gethostbyaddr(ip_address)
+        return hostname[0]
+    except socket.herror as err:
+        return f"Erreur: Impossible de résoudre le nom pour {ip_address} -> {err}"
 
 def requete(ip):
     try:
@@ -25,12 +22,10 @@ def requete(ip):
             hostname = get_hostname(ip)
             with open("scanip.txt", "a") as f:
                 f.write(f"{ip} -> {hostname}\n")
-                f.close
             return f"{ip} -> {hostname}"
         else:
-            with open("scanip.txt", "a") as f: 
+            with open("scanip.txt", "a") as f:
                 f.write(f"{ip} -> X\n")
-                f.close
             return f"{ip} -> X"
 
     except subprocess.CalledProcessError:
@@ -39,32 +34,27 @@ def requete(ip):
 def multi_requete(liste):
     with concurrent.futures.ThreadPoolExecutor() as executor:
         futures = [executor.submit(requete, ip) for ip in liste]
-        
+
         for future, ip in zip(futures, liste):
             result = future.result()
             print(result)
-
-
 
 result_ip_dep = requete(ip_dep)
 print(result_ip_dep)
 
 ip = ip_dep
-ip_fin_parts = list(map(int, ip_fin.split('.'))) 
+ip_fin_parts = list(map(int, ip_fin.split('.')))
 
 while ip != ip_fin:
     listeip = []
     ip_parts = list(map(int, ip.split('.')))
 
-    while ip_parts[2] <= ip_fin_parts[2] and ip != ip_fin: 
+    while ip_parts[2] <= ip_fin_parts[2] and ip != ip_fin:
         for _ in range(255):
-            if ip_parts[3] >= ip_fin_parts[3] or ip == ip_fin:
+            if ip_parts[3] >= ip_fin_parts[3] or ip == f"{ip_parts[0]}.{ip_parts[1]}.{ip_parts[2]}.{ip_parts[3] + 1}":
                 break
-            elif ip_parts[3] < 255 and ip != f"{ip_parts[0]}.{ip_parts[1]}.{ip_parts[2]}.{ip_parts[3]+1}":
-                ip_parts[3] += 1
             else:
-                ip_parts[3] = 0
-                ip_parts[2] += 1
+                ip_parts[3] += 1
 
             ip = ".".join(map(str, ip_parts))
             listeip.append(ip)
